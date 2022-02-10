5ac782aec7c16f1f886599a6a708587d4b3b3d84
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-02-09 22:25:26 -0500

    Intermediate commit.

--- fhirlib/test/ConceptIRI.test.js
+++ fhirlib/test/ConceptIRI.test.js
@@ -1,4 +1,5 @@
-let { ConceptIRI } = require('../ConceptIRI.js');
+const { ConceptIRI } = require('../ConceptIRI.js');
+const lodash = require('lodash');
 const path = require("path");
 const fs = require("fs");
 
@@ -45,16 +46,64 @@ test('Convert some Concept IRIs to FHIR Codings', () => {
     }
 });
 
-// Let's test every system/value pair in the JSON file.
-test('Test every system/value pair in the FHIR JSON examples', () => {
-    let package_json_path = require.resolve('hl7.terminology/package.json');
-    if (!package_json_path) {
-      throw new Error("ConceptIRI requires 'hl7.terminology' to be installed.");
+// Return all JSON files recursively in a directory.
+function get_files_in_dir(dirname, filter=(filename) => true, recursive=true) {
+    let files = fs.readdirSync(dirname);
+    let output_files = [];
+    files.forEach(file => {
+       let filename = path.resolve(dirname, file);
+       let ls = fs.lstatSync(filename);
+       if(ls.isDirectory()) {
+           if (recursive)
+               output_files = output_files.concat(get_files_in_dir(filename, filter, recursive));
+           else
+               if (filter(filename)) output_files.push(filename);
+       } else if(ls.isFile()) {
+            if (filter(filename)) output_files.push(filename);
+       } else {
+           throw new Error(`File ${file} in directory ${dirname} is neither a file nor a directory.`);
+       }
+    });
+    return output_files;
+}
+
+function get_system_code_pairs_from_file(filename, inner_object = undefined) {
+    // The conventional thing to do would be to find all the Codings; however, to
+    // ensure that we don't miss anything, I'm just going to search recursively for
+    // every system/code pair anywhere in any of these files.
+
+    let obj = inner_object;
+    if (!obj) {
+        obj = JSON.parse(fs.readFileSync(filename));
     }
-    let hl7terminology_path = path.dirname(package_json_path);
 
-    // Load all CodeSystem and Naming files from the hl7terminology path and look for prefix information.
-    let files = fs.readdirSync(hl7terminology_path);
+    // Does obj have a system/code pair?
+    if (Object.hasOwnProperty())
 
-    // TODO: use glob() to find all JSON files and stuff.
+}
+
+/*
+ * Test for all the files in a FHIR JSON examples directory.
+ * The path should either point to the v4 or v5 directory.
+ */
+function test_fhir_json(fhir_json_path) {
+    // Step 1.  Find every system/code pair in the FHIR JSON examples.
+    // The conventional thing to do would be to find all the Codings; however, to
+    // ensure that we don't miss anything, I'm just going to search recursively for
+    // every system/code pair anywhere in any of these files.
+    let fhir_jsons = get_files_in_dir(fhir_json_path, (filename) => filename.toLowerCase().endsWith('.json'), true);
+
+    // Step 2. Open each file and recurse through every object.
+    let system_code_pairs = fhir_jsons.flatMap(get_system_code_pairs_from_file);
+}
+
+// Let's test every system/value pair in the FHIR JSON R4 files.
+test('Test every system/value pair in the FHIR JSON R4 examples', () => {
+    test_fhir_json(path.resolve(__dirname, 'fhir/examples/fhir-r4'));
 });
+
+// Let's test every system/value pair in the FHIR JSON R5 files.
+test('Test every system/value pair in the FHIR JSON R5 examples', () => {
+    test_fhir_json(path.resolve(__dirname, 'fhir/examples/fhir-r5'));
+});
+
